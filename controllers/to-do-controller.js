221b4a1cb244e3e9e345e9cc1ec26dcfd061cbde
nimesh <2018cs131@stu.ucsc.cmb.ac.lk>
2021-09-05 18:03:52 +0530

    x

---
+++ controllers/to-do-controller.js
@@ -0,0 +1,14 @@
+//manipulate data, handle routes , passing data to view etc
+module.exports = (app)=>{
+    app.get('/todo', (req, res)=>{
+
+    }) //request handlers
+
+    app.post('/todo', (req, res)=>{
+        
+    })
+
+    app.delete('/todo', (req, res)=>{
+
+    })
+}
