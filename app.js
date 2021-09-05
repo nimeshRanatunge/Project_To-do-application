b1da8f9a38ffb237a91e2ee55b66319a79f2ae64
nimesh <2018cs131@stu.ucsc.cmb.ac.lk>
2021-09-05 17:07:23 +0530

    x

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express')
+
+var todoController = require('./controllers/to-do-controller')
+
+var app = express()
+
+//set up template engine
+app.set('view engine', 'ejs')
+
+//inbuilt express middleware (EXPRESS STATIC)
+app.use('assets', express.static('./public')) //when go to assets route it maps to static file  
+
+//listen to a port
+app.listen(3000)
+console.log('you are listning to port 3000')
