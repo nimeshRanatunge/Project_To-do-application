fa040af6c8d0d9c7d8ab267c86c4eb5187ab93b7
nimesh <2018cs131@stu.ucsc.cmb.ac.lk>
2021-09-06 10:37:05 +0530

    x

--- controllers/to-do-controller.js
+++ controllers/to-do-controller.js
@@ -1,13 +1,19 @@
 //manipulate data, handle routes , passing data to view etc
+var bodyParser = require('body-parser') // middleware
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'run fast'}]
+var urlencodedParser = bodyParser.urlencoded({ extended: false }) // middleware that we want to run in post method
 
 module.exports = (app)=>{
     app.get('/todo', (req, res)=>{
-        res.render('to-do', {todos: data})
+        res.render('todo', {todos: data})
     }) //request handlers
 
-    app.post('/todo', (req, res)=>{
-        
+    app.post('/todo', urlencodedParser, (req, res)=>{
+        //add body-parser middleware
+        //grab data in req body and push that data to data array
+        data.push(req.body)
+        //send this data to frontend
+        res.json(data)
     })
 
     app.delete('/todo', (req, res)=>{
